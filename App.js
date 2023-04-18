7458d8dd88c2c730e30bb33c4223cc25dec762e6
EacAnu <131151810+EacAnu@users.noreply.github.com>
2023-04-18 03:57:51 -0700

    Update App.js

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import {  View } from 'react-native';
 import { UI } from './Components/UI';
 
 export default function App() {
+//   Testing GitHub
   return (
     <View style={{flex:1}}>
       < UI/>
